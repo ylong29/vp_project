72bb120cf65dd988bd1de7f3cce425f2836672bd
ylong29 <95624093+ylong29@users.noreply.github.com>
2022-11-13 00:14:36 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,10 +4,11 @@ import UE4VirtualProduction
 
 class Main(object):
     def __init__(self, input_1, input_2, output):
-        tracker = Tracker(input_1, input_2, output)
-        vp_studio = VPStudio(tracker)
-        vp = UE4VirtualProduction(vp_studio)
+        self.plan = Plan(input_1, input_2, output)
+        self.tracker = Tracker(self.plan)
+        self.vp_studio = VPStudio(self.tracker)
+        self.vp = UE4VirtualProduction(self.vp_studio)
 
     def process(self):
-        vp.forward()
-        vp.update()
+        self.vp.forward()
+        self.vp.update()
