f2f2dc4cf2f8f85c0b837eccf7b49d90b795947e
ylong29 <95624093+ylong29@users.noreply.github.com>
2022-11-13 21:17:46 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,14 @@
+import EnterpriseResourcePlan
+import Production
 import Tracker
 import VPStudio
 import UE4VirtualProduction
 
 class Main(object):
     def __init__(self, input_1, input_2, output):
-        self.plan = Plan(input_1, input_2, output)
-        self.tracker = Tracker(self.plan)
+        self.plan = EnterpriseResourcePlan(input_1, input_2, output)
+        self.production = Production(input_1, input_2, output)
+        self.tracker = Tracker(self.plan, self.production)
         self.vp_studio = VPStudio(self.tracker)
         self.vp = UE4VirtualProduction(self.vp_studio)
 
