4bbdb486d43ca374ce300dacd6313ebc51827c2b
ylong29 <95624093+ylong29@users.noreply.github.com>
2022-11-13 21:17:20 +0800

    Update VPStudio.py

--- VPStudio/VPStudio.py
+++ VPStudio/VPStudio.py
@@ -1,45 +1,43 @@
 import numpy as np
 from functools import reduce
-import UE4VirtualProduction as vp
+import VPStudio
+import Tracker
 
 class VPStudio(object):
-    def __init__(self, input_width, state_width,
-                 activator, learning_rate):
-        self.input_width = input_width
+    def __init__(self, tracker):
+        self.tracker = tracker
+        self.vp_studio = VPStudio()
         self.state_width = state_width
         self.activator = activator
         self.learning_rate = learning_rate
-        self.times = 0  # 当前时刻初始化为t0
-        self.state_list = []  # 保存各个时刻的state
+        self.times = 0 
+        self.state_list = []  
         self.state_list.append(np.zeros(
-            (state_width, 1)))  # 初始化s0
+            (state_width, 1)))  
         self.U = np.random.uniform(-1e-4, 1e-4,
-                                   (state_width, input_width))  # 初始化U
+                                   (state_width, input_width))  
         self.W = np.random.uniform(-1e-4, 1e-4,
-                                   (state_width, state_width))  # 初始化W
+                                   (state_width, state_width))  
 
-    def forward(self, input_array):
+    def forward(self):
         self.times += 1
         state = (np.dot(self.U, input_array) +
                  np.dot(self.W, self.state_list[-1]))
         element_wise_op(state, self.activator.forward)
-        self.state_list.append(state)
+        self.tracker.set_enterprise_resource_plan(state)
 
-    def backward(self, sensitivity_array,
-                 activator):
-        self.calc_delta(sensitivity_array, activator)
-        self.calc_gradient()
+    def backward(self):
+        self.tracker.set_plan_schedule()
 
-    def update(self):
-        self.W -= self.learning_rate * self.gradient
-
-    def calc_delta(self, sensitivity_array, activator):
-        self.delta_list = []  # 用来保存各个时刻的误差项
+    def process(self, sensitivity_array, activator):
+        self.delta_list = []  
+        sensitivity_array = []
+        activator = []
         for i in range(self.times):
             self.delta_list.append(np.zeros(
                 (self.state_width, 1)))
         self.delta_list.append(sensitivity_array)
-        # 迭代计算每个时刻的误差项
+        
         for k in range(self.times - 1, 0, -1):
             self.calc_delta_k(k, activator)
 
@@ -52,27 +50,27 @@ class VPStudio(object):
             np.diag(state[:, 0])).T
 
     def calc_gradient(self):
-        self.gradient_list = []  # 保存各个时刻的权重梯度
+        self.gradient_list = []  
         for t in range(self.times + 1):
             self.gradient_list.append(np.zeros(
                 (self.state_width, self.state_width)))
         for t in range(self.times, 0, -1):
             self.calc_gradient_t(t)
-        # 实际的梯度是各个时刻梯度之和
+        
         self.gradient = reduce(
             lambda a, b: a + b, self.gradient_list,
-            self.gradient_list[0])  # [0]被初始化为0且没有被修改过
+            self.gradient_list[0])  
 
     def calc_gradient_t(self, t):
-        '''
-        计算每个时刻t权重的梯度
-        '''
         gradient = np.dot(self.delta_list[t],
                           self.state_list[t - 1].T)
         self.gradient_list[t] = gradient
+        self.vp_studio.calculate_yirtual_production(self.gradient_list)
 
     def reset_state(self):
-        self.times = 0  # 当前时刻初始化为t0
-        self.state_list = []  # 保存各个时刻的state
+        self.delta_list = []  
+        sensitivity_array = []
+        activator = []
+        self.state_list = []  
         self.state_list.append(np.zeros(
-            (self.state_width, 1)))  # 初始化s0
+            (self.state_width, 1)))  
